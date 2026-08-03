a7f1bdd13089dfa1fc8e76cea927bef155be2a91
tanhung-05 <hungluong2706@gmail.com>
2026-08-03 23:45:03 +0700

    fix(deploy): make admin promotion compatible with indexes

--- FashionHub2/scripts/promote-admin.sh
+++ FashionHub2/scripts/promote-admin.sh
@@ -13,7 +13,22 @@ if [[ ! "${email}" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
     exit 1
 fi
 
-sql="USE [QL_SHOPQUANAO_PRO]; UPDATE dbo.NguoiDung SET IDVaiTro = 1 WHERE Email = N'${email}'; IF @@ROWCOUNT <> 1 THROW 51000, 'Registered user not found or email is not unique.', 1;"
+sql="
+SET ANSI_NULLS ON;
+SET ANSI_PADDING ON;
+SET ANSI_WARNINGS ON;
+SET ARITHABORT ON;
+SET CONCAT_NULL_YIELDS_NULL ON;
+SET QUOTED_IDENTIFIER ON;
+SET NUMERIC_ROUNDABORT OFF;
+USE [QL_SHOPQUANAO_PRO];
+UPDATE dbo.NguoiDung
+SET IDVaiTro = 1,
+    SecurityStamp = NEWID(),
+    NgayCapNhat = SYSDATETIME()
+WHERE Email = N'${email}';
+IF @@ROWCOUNT <> 1
+  THROW 51000, 'Registered user not found or email is not unique.', 1;"
 
 # Expansion is intentionally deferred to bash inside the SQL container.
 # shellcheck disable=SC2016
