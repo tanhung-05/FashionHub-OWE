35d40142f510720917b67c3a5159d3154734240e
tanhung-05 <hungluong2706@gmail.com>
2026-08-03 23:56:23 +0700

    fix(web): restore production client interactions under CSP

--- FashionHub2/FashionHub.Tests/Controllers/PublicPagesControllerTests.cs
+++ FashionHub2/FashionHub.Tests/Controllers/PublicPagesControllerTests.cs
@@ -35,4 +35,23 @@ public class PublicPagesControllerTests
         content.Should().Contain("tel:0392410917");
         content.Should().Contain("https://zalo.me/0392410917");
     }
+
+    [Fact]
+    public async Task Home_UsesAvailableLocalCoreClientAssets()
+    {
+        var response = await client.GetAsync("/");
+
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        content.Should().Contain("/lib/jquery/dist/jquery.min.js");
+        content.Should().Contain("/lib/bootstrap/dist/js/bootstrap.bundle.min.js");
+        content.Should().Contain("/lib/bootstrap/dist/css/bootstrap.min.css");
+        content.Should().NotContain("https://code.jquery.com");
+
+        var jqueryResponse = await client.GetAsync("/lib/jquery/dist/jquery.min.js");
+        var bootstrapResponse = await client.GetAsync("/lib/bootstrap/dist/js/bootstrap.bundle.min.js");
+
+        jqueryResponse.EnsureSuccessStatusCode();
+        bootstrapResponse.EnsureSuccessStatusCode();
+    }
 }
--- FashionHub2/FashionHub.Web/Program.cs
+++ FashionHub2/FashionHub.Web/Program.cs
@@ -378,10 +378,10 @@ app.Use(async (context, next) =>
         context.Response.Headers.Append("Content-Security-Policy", 
             "default-src 'self'; " +
             "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://generativelanguage.googleapis.com; " +
-            "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; " +
+            "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com; " +
             "img-src 'self' data: https:; " +
-            "font-src 'self' https://cdn.jsdelivr.net; " +
-            "connect-src 'self' https://generativelanguage.googleapis.com;");
+            "font-src 'self' data: https://cdn.jsdelivr.net https://fonts.gstatic.com; " +
+            "connect-src 'self' https://cdn.jsdelivr.net https://fonts.googleapis.com https://fonts.gstatic.com https://generativelanguage.googleapis.com;");
     }
     
     await next();
